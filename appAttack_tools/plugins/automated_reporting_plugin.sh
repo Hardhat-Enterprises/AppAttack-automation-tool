6dcfae9df3ede3462de97be631ba2c95d6eba13c
HackerBree <kagwe.bridgette@gmail.com>
2026-04-18 09:28:56 +1000

    Update automated_reporting_plugin.sh

--- appAttack_tools/plugins/automated_reporting_plugin.sh
+++ appAttack_tools/plugins/automated_reporting_plugin.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-# Automated Reporting Plugin for AppAttack toolkit
-# 
-# WHAT THIS DOES:
-# After you run security scans, this takes all the result files
-# and bundles them into ONE webpage you can open in your browser.
 
-# This is just the name of this tool
+# Automated Reporting Plugin for AppAttack toolkit
+
+# WHAT THIS DOES:
+# After the "Penetration Testing Team" runs security scans, this takes all the result files and bundles them into ONE .html file you can open in your browser.
+
+# This is just the name of this tool.
+
 TOOL_NAME="AutomatedReporting"
 
-# This is the main action that runs when you pick this plugin
+# This is the main action that runs when you pick this plugin.
 run_plugin() {
     echo "[Automated Reporting Plugin] Aggregating scan outputs..."
     
-    # Make a folder to store all reports (if it doesn't exist yet)
-    # $HOME just means "your home folder"
+    # Make a folder to store all reports (if it doesn't exist yet).
+    # $HOME just means "your home folder".
     report_dir="$HOME/appattack_reports"
     mkdir -p "$report_dir"
     
-    # Create a file name that includes the current date and time
+    # Create a file name that includes the current date and time, you can find this files in the GitHub repo under reports.
     # Example: scan_report_20260116_143022.html
-    # This way every report has a unique name and you won't overwrite old ones
+    # This way every report has a unique name and you won't overwrite old ones.
     report_file="$report_dir/scan_report_$(date +%Y%m%d_%H%M%S).html"
     
-    # Start building the webpage
+    # Start building the .html file.
     # The > symbol means "write this into the file" (start fresh)
     echo "<html><head><title>AppAttack Scan Report</title></head><body>" > "$report_file"
     echo "<h1>AppAttack Scan Report</h1>" >> "$report_file"  # >> means "add this to the end"
@@ -39,7 +40,7 @@ run_plugin() {
         fi
     done
     
-    # Finish the webpage
+    # Finish the .html file
     echo "</body></html>" >> "$report_file"
     
     # Tell the user where to find their report
