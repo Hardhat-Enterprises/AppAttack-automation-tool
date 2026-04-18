82d9073f44cd2ff396ab98d81bbc417a8bb5bd81
HackerBree <kagwe.bridgette@gmail.com>
2026-04-18 10:00:25 +1000

    Update sample_plugin.sh

--- appAttack_tools/plugins/sample_plugin.sh
+++ appAttack_tools/plugins/sample_plugin.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-# Plugin interface for AppAttack toolkit
-# 
+# Plugin interface for AppAttack toolkit.
+
 # WHAT THIS IS:
-# This is a BLANK TEMPLATE for creating new plugins.
-# Think of it like a form you fill out to add a new tool to AppAttack.
-# 
+# This is a BLANK TEMPLATE for creating new plugins, if you want to create a new plugin use this file as a template.
+
 # REQUIRED RULES:
 # Every plugin MUST have:
-# 1. A TOOL_NAME (what your tool is called)
-# 2. A run_plugin function (what happens when someone picks your tool)
-# 
+# 1. A TOOL_NAME (what your tool is called).
+# 2. A run_plugin function (what happens when someone picks your tool).
+
 # OPTIONAL:
-# You can also add a plugin_help function (shows a description)
+# You can also add a plugin_help function (shows a description of what the file does).
 
 # ----------------------------------------------------------------------
-# STEP 1: Give your plugin a name
-# Change "SamplePlugin" to whatever your tool is called
-# Example: TOOL_NAME="MyCoolScanner"
+# STEP 1: Give your plugin a name.
+# Change "SamplePlugin" to whatever your tool is called.
+# Example: TOOL_NAME="MyCoolScanner".
 # ----------------------------------------------------------------------
 TOOL_NAME="SamplePlugin"
 
 # ----------------------------------------------------------------------
-# STEP 2: Write what your plugin actually DOES
-# This function runs when someone selects your plugin from the menu
-# Put your tool's commands between the { and the }
+# STEP 2: Write the code of what your plugin actually DOES.
+# This function runs when someone selects your plugin from the menu.
+# Put your tool's commands between the { and the }.
 # ----------------------------------------------------------------------
 run_plugin() {
     echo "Running $TOOL_NAME plugin..."
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    # TODO: Delete the line above and put YOUR tool's code here instead
-    #
-    # Examples of what you might put here:
-    # - Run a security scanner:  nmap -sV 192.168.1.1
-    # - Check a website:         curl https://example.com
-    # - Run a Python script:     python3 my_scanner.py
-    # - Analyze a file:          cat results.txt
-    #
+    # Delete this lines above and below and put YOUR tool's code here instead.
     # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 }
 
@@ -45,6 +37,6 @@ run_plugin() {
 # ----------------------------------------------------------------------
 plugin_help() {
     echo "This is a sample plugin for AppAttack toolkit."
-    # TODO: Change this to describe what YOUR tool actually does
-    # Example: echo "Scans a website for common security problems"
+    # Change this to describe what YOUR tool actually does.
+    # Example: echo "Scans a website for common security problems".
 }
